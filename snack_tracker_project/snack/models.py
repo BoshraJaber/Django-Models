8ba5c71a374c1d7172885d025f4eaa80d21e1d23
BoshraJaber <boshrajaber94@gmail.com>
2021-10-26 16:59:33 +0300

    created the model

--- snack_tracker_project/snack/models.py
+++ snack_tracker_project/snack/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Snack(models.Model):
+    name = models.CharField(max_length=255)
+    rating = models.IntegerField(default=0)
+    # get_user_model will give us the name of the table, it returns the name of a class  
+    reviewer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+# it will take the name from the Snack class
+# delete CASCADE it means it will delete in childern as well, same goes to update
+
